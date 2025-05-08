49f645943c2e3b8dd0ba3a295827a85a88372e31
Nihal <96400699+nihal370@users.noreply.github.com>
2025-05-08 07:00:05 +0530

    Update error_handle.py

--- error_handle.py
+++ error_handle.py
@@ -1,9 +1,9 @@
 file = open('youtube.txt', 'w')
 
 try:
-    file.write('chai aur code')
+    file.write('code')
 finally:
     file.close()
 
 with open('youtube.txt', 'w') as file:
-    file.write('chai aur python')
+    file.write('python')
